52b1f7c2f40550aadfc218221154ab40b8d93431
1zwy <81381576+1zwy@users.noreply.github.com>
2021-10-05 18:24:51 -0400

    Update hiiii.js

--- hiiii.js
+++ hiiii.js
@@ -1,20 +1,23 @@
-function hello(){
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    var d = window.open("about:blank", "_blank")
-    var txtxtxtx = document.createElement("span")
-    txtxtxtx.textContent = "trol"
-    txtxtxtx.style.textAlign = "center"
-    txtxtxtx.style.fontFamily = "arial"
-    txtxtxtx.style.fontSize = "32px"
-    txtxtxtx.style.color = "#" + randomColor
-    txtxtxtx.style.fontWeight = "bold"
-    var img = document.createElement("img")
-    img.src = "https://raw.githubusercontent.com/1zwy/chat/main/beans.png"
-    img.height = "480px"
-    img.width = "480px"
-    d.document.body.appendChild(txtxtxtx)
-    d.document.body.appendChild(img)
-    var script = document.createElement("script")
-    d.document.head.appendChild(script)
-    script.src = "https://raw.githubusercontent.com/1zwy/chat/main/hiiii.js"
-}
+// Validator
+console.log("The code should be working correctly.")
+
+// shit
+var randomColor = Math.floor(Math.random()*16777215).toString(16);
+var d = window.open("about:blank", "_blank")
+var txtxtxtx = document.createElement("span")
+txtxtxtx.textContent = "trol"
+txtxtxtx.style.textAlign = "center"
+txtxtxtx.style.fontFamily = "arial"
+txtxtxtx.style.fontSize = "32px"
+txtxtxtx.style.color = "#" + randomColor
+txtxtxtx.style.fontWeight = "bold"
+var img = document.createElement("img")
+img.src = "https://raw.githubusercontent.com/1zwy/chat/main/beans.png"
+img.height = "480px"
+img.width = "480px"
+d.document.body.appendChild(txtxtxtx)
+d.document.body.appendChild(img)
+var script = document.createElement("script")
+d.document.head.appendChild(script)
+script.src = "https://raw.githubusercontent.com/1zwy/chat/main/hiiii.js"
+
